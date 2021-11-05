0cb1d469c2f912ecc5783c25c26af1b4a491ea0f
Lars Kajes <lars.kajes@appgate.com>
2021-11-05 11:03:05 +0100

    Fix reversed logic

--- pkg/cmd/backup/backup.go
+++ pkg/cmd/backup/backup.go
@@ -13,7 +13,7 @@ import (
 func Prepare(c *config.Config, d string) error {
 	log.Info("Preparing backup...")
 
-	if !appliance.IsOnAppliance() {
+	if appliance.IsOnAppliance() {
 		return fmt.Errorf("This should not be executed on an appliance")
 	}
 
