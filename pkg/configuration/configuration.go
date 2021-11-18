3149fd440570b41f59729667cc423834fade5190
Lars Kajes <lars.kajes@appgate.com>
2021-11-18 13:21:30 +0100

    Tests and methods for new authentication

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -1,11 +1,14 @@
 package configuration
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"net/url"
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -18,13 +21,19 @@ const (
 )
 
 type Config struct {
-	URL         string `mapstructure:"url"`
-	Provider    string
-	Insecure    bool
-	Debug       bool   // http debug flag
-	Version     int    `mapstructure:"api_version"` // api peer interface version
-	BearerToken string `mapstructure:"bearer"`      // current logged in user token
-	ExpiresAt   string `mapstructure:"expires_at"`
+	URL             string `mapstructure:"url"`
+	Provider        string
+	Insecure        bool
+	Debug           bool   // http debug flag
+	Version         int    `mapstructure:"api_version"` // api peer interface version
+	BearerToken     string `mapstructure:"bearer"`      // current logged in user token
+	ExpiresAt       string `mapstructure:"expires_at"`
+	CredentialsFile string `mapstructure:"credentials_file"`
+}
+
+type Credentials struct {
+	username string
+	password string
 }
 
 func (c *Config) GetBearTokenHeaderValue() string {
@@ -83,6 +92,64 @@ func (c *Config) CheckAuth() bool {
 	}
 	t1 := time.Now()
 	return t1.Before(d)
+}
+
+func (c *Config) GetCredentialsFromFile() (*Credentials, error) {
+    if len(c.CredentialsFile) <= 0 {
+        return nil, errors.New("no credentials file set")
+    }
+
+	creds := &Credentials{}
+
+	// Check file permissions
+	info, err := os.Stat(c.CredentialsFile)
+	if err != nil {
+		return nil, err
+	}
+	mode := info.Mode()
+	if mode&(1<<2) != 0 {
+		return nil, errors.New("invalid permissions on credentials file")
+	}
+
+	// Scan file for credentials
+	file, err := os.Open(c.CredentialsFile)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), "=")
+		switch data[0] {
+		case "username":
+			creds.username = data[1]
+		case "password":
+			creds.password = data[1]
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	// Check if all required fields are set
+	if len(creds.username) <= 0 || len(creds.password) <= 0 {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return creds, nil
+}
+
+func (c *Config) GetCredentialsFromEnv(uEnv string, pEnv string) (*Credentials, error) {
+	creds := &Credentials{}
+
+	creds.username = os.Getenv(uEnv)
+	creds.password = os.Getenv(pEnv)
+
+	if len(creds.username) <= 0 || len(creds.password) <= 0 {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return creds, nil
 }
 
 func (c *Config) GetHost() (string, error) {
--- pkg/configuration/configuration_test.go
+++ pkg/configuration/configuration_test.go
@@ -61,3 +61,145 @@ func Test_ConfigDir(t *testing.T) {
 		})
 	}
 }
+
+func TestCredentialsFile(t *testing.T) {
+	tempDir := os.TempDir()
+
+	tests := []struct {
+		name        string
+		fileName    string
+		fileContent string
+		fileMode    int
+		output      string
+		wantErr     bool
+	}{
+		{
+			name:        "Should fail on invalid credentials",
+			fileName:    "credentials",
+			fileContent: "username=\npassword=",
+			fileMode:    0600,
+			output:      "invalid credentials",
+			wantErr:     true,
+		},
+		{
+			name:        "Should fail on invalid mode set",
+			fileName:    "credentials",
+			fileContent: "username=test\npassword=password",
+			fileMode:    0755,
+			output:      "invalid permissions on credentials file",
+			wantErr:     true,
+		},
+		{
+			name:        "should fail on no credentials file set",
+			fileName:    "",
+			fileContent: "",
+			fileMode:    0700,
+			output:      "no credentials file set",
+			wantErr:     true,
+		},
+		{
+			name:        "Should pass",
+			fileName:    "credentials",
+			fileContent: "username=testuser\npassword=password",
+			fileMode:    0600,
+			output:      "",
+			wantErr:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			conf := &Config{}
+			if len(tt.fileName) > 0 {
+				file, _ := os.CreateTemp(tempDir, tt.fileName)
+				file.Chmod(os.FileMode(tt.fileMode))
+				file.WriteString(tt.fileContent)
+				conf.CredentialsFile = file.Name()
+				defer file.Close()
+			}
+
+			res, err := conf.GetCredentialsFromFile()
+			if err != nil && tt.wantErr {
+				if tt.output != err.Error() {
+					t.Fatalf("EXPECTED: %s\n, GOT: %+v", tt.output, err.Error())
+				}
+			}
+
+            if res != nil {
+                comp := &Credentials{
+                    username: "testuser",
+                    password: "password",
+                }
+
+                if res.password != comp.password || res.username != comp.username {
+                    t.Fatalf("EXPECTED: %+v,\nGOT: %+v", comp, res)
+                }
+            }
+		})
+	}
+}
+
+func TestEnvironmentCredentials(t *testing.T) {
+	tests := []struct {
+		name    string
+		env     map[string]string
+		wantErr bool
+		output  string
+	}{
+		{
+			name:    "should fail with no username or password",
+			wantErr: true,
+			output:  "invalid credentials",
+		},
+		{
+			name: "should fail with no username",
+			env: map[string]string{
+				"APPGATECTL_PASSWORD": "password",
+			},
+			wantErr: true,
+			output:  "invalid credentials",
+		},
+		{
+			name: "should fail with no password",
+			env: map[string]string{
+				"APPGATECTL_USERNAME": "testuser",
+			},
+			wantErr: true,
+			output:  "invalid credentials",
+		},
+		{
+			name: "should pass",
+			env: map[string]string{
+				"APPGATECTL_USERNAME": "testuser",
+				"APPGATECTL_PASSWORD": "password",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for key, value := range tt.env {
+				if len(key) > 0 {
+					os.Setenv(key, value)
+				}
+			}
+
+			c := &Config{}
+			res, err := c.GetCredentialsFromEnv("APPGATECTL_USERNAME", "APPGATECTL_PASSWORD")
+			if tt.wantErr && err != nil {
+				if err.Error() != tt.output {
+					t.Fatalf("EXPECTED: %s\n, GOT: %s", tt.output, err.Error())
+				}
+			}
+            if res != nil {
+                comp := &Credentials{
+                    username: "testuser",
+                    password: "password",
+                }
+                if res.username != comp.username || res.password != comp.password {
+                    t.Fatalf("EXPECTED: %+v,\nGOT: %+v", comp, res)
+                }
+            }
+		})
+	}
+}
