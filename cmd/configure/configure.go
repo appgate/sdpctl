5d5b5b031a29acfc37f71144d8f38b8d12eed629
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-12-01 13:35:31 +0100

    use machine ID as device ID

--- cmd/configure/configure.go
+++ cmd/configure/configure.go
@@ -6,14 +6,12 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/appgate/appgatectl/pkg/configuration"
 	"github.com/appgate/appgatectl/pkg/factory"
+	"github.com/denisbrodbeck/machineid"
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
-
-// DefaultAPIVersion is based on the latest peer api version.
-const DefaultAPIVersion = 16
 
 type configureOptions struct {
 	Config *configuration.Config
@@ -74,12 +72,38 @@ func configRun(cmd *cobra.Command, args []string, opts *configureOptions) error 
 	viper.Set("url", answers.URL)
 	i, _ := strconv.ParseBool(answers.Insecure)
 	viper.Set("insecure", i)
-	dID := uuid.New()
-	viper.Set("device_id", dID)
+	viper.Set("device_id", defaultDeviceID())
 
 	if err := viper.WriteConfig(); err != nil {
 		return err
 	}
 	log.Infof("Config updated %s", viper.ConfigFileUsed())
 	return nil
+}
+
+func defaultDeviceID() string {
+	readAndParseUUID := func() (string, error) {
+		// machine.ID() tries to read
+		// /etc/machine-id on Linux
+		// /etc/hostid on BSD
+		// ioreg -rd1 -c IOPlatformExpertDevice | grep IOPlatformUUID on OSX
+		// reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography /v MachineGuid on Windows
+		// and tries to parse the value as a UUID
+		// https://github.com/denisbrodbeck/machineid
+		id, err := machineid.ID()
+		if err != nil {
+			return "", err
+		}
+		uid, err := uuid.Parse(id)
+		if err != nil {
+			return "", err
+		}
+		return uid.String(), nil
+	}
+	// if we cant get a valid UUID based on the machine ID, we will fallback to a random UUID value.
+	v, err := readAndParseUUID()
+	if err != nil {
+		return uuid.New().String()
+	}
+	return v
 }
