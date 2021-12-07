fd67049d5bc133e62bca6101449afc466c0616a2
Daniel Nilsson <daniel.nilsson@appgate.com>
2021-12-07 16:30:14 +0100

    completion command

---
+++ cmd/completion.go
@@ -0,0 +1,67 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// NewCmdCompletion represents the completion command
+func NewCmdCompletion() *cobra.Command {
+	var completionCmd = &cobra.Command{
+		Use: "completion",
+		Annotations: map[string]string{
+			"skipAuthCheck": "true",
+		},
+		Short: "Generate shell completion scripts",
+		Long: `To load completions:
+
+Bash:
+
+  $ source <(appgatectl completion bash)
+
+  # To load completions for each session, execute once:
+  # Linux:
+  $ appgatectl completion bash > /etc/bash_completion.d/appgatectl
+  # macOS:
+  $ appgatectl completion bash > /usr/local/etc/bash_completion.d/appgatectl
+
+Zsh:
+
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
+
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+  # To load completions for each session, execute once:
+  $ appgatectl completion zsh > "${fpath[1]}/_appgatectl"
+
+  # You will need to start a new shell for this setup to take effect.
+
+
+PowerShell:
+
+  PS> appgatectl completion powershell | Out-String | Invoke-Expression
+
+  # To load completions for every new session, run:
+  PS> appgatectl completion powershell > appgatectl.ps1
+  # and source this file from your PowerShell profile.
+`,
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "powershell"},
+		Args:                  cobra.ExactValidArgs(1),
+		Example:               "appgatectl completion bash",
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				cmd.Root().GenBashCompletion(os.Stdout)
+			case "zsh":
+				cmd.Root().GenZshCompletion(os.Stdout)
+			case "powershell":
+				cmd.Root().GenPowerShellCompletionWithDesc(os.Stdout)
+			}
+		},
+	}
+
+	return completionCmd
+}
