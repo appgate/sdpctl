5194f35b7ea5220901765358ee00fa66c54bf4ad
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-01-03 16:03:39 +0100

    toggle backup API with new command
    Example usage and output
    
    ```bash
    > appgatectl appliance backup api
    Backup api is already enabled.
    ```
    
    ```bash
    > appgatectl appliance backup api --disable
    backup API has been disabled.
    ```
    
    ```bash
    > appgatectl appliance backup api
    ? The passphrase to encrypt Appliance Backups when backup API is used: ******
    Backup API and phassphrase has been updated.
    ```

---
+++ cmd/appliance/backup/api_test.go
@@ -0,0 +1,177 @@
+package backup
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/appgate/appgatectl/pkg/appliance"
+	"github.com/appgate/appgatectl/pkg/configuration"
+	"github.com/appgate/appgatectl/pkg/factory"
+	"github.com/appgate/appgatectl/pkg/httpmock"
+	"github.com/appgate/appgatectl/pkg/prompt"
+	"github.com/appgate/sdp-api-client-go/api/v16/openapi"
+)
+
+func TestBackupAPICommandAlreadyEnabled(t *testing.T) {
+	registry := httpmock.NewRegistry()
+	registry.Register(
+		"/global-settings",
+		func(rw http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodGet {
+				rw.Header().Set("Content-Type", "application/json")
+				rw.WriteHeader(http.StatusOK)
+				fmt.Fprint(rw, string(`{
+                    "claimsTokenExpiration": 1440,
+                    "entitlementTokenExpiration": 180,
+                    "administrationTokenExpiration": 720,
+                    "vpnCertificateExpiration": 525600,
+                    "spaMode": "TCP",
+                    "loginBannerMessage": "Authorized use only.",
+                    "messageOfTheDay": "Welcome to Appgate SDP.",
+                    "backupApiEnabled": true,
+                    "fips": false,
+                    "geoIpUpdates": false,
+                    "auditLogPersistenceMode": "Default",
+                    "appDiscoveryDomains": [
+                      "company.com"
+                    ],
+                    "collectiveId": "4c07bc69-57ea-42dd-b702-c2d6c45419fc"
+                  }
+                `))
+			}
+		},
+	)
+	defer registry.Teardown()
+	registry.Serve()
+	stdout := &bytes.Buffer{}
+	stdin := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	in := io.NopCloser(stdin)
+	f := &factory.Factory{
+		Config: &configuration.Config{
+			Debug: false,
+			URL:   fmt.Sprintf("http://localhost:%d", registry.Port),
+		},
+		IOOutWriter: stdout,
+		Stdin:       in,
+		StdErr:      stderr,
+	}
+	f.APIClient = func(c *configuration.Config) (*openapi.APIClient, error) {
+		return registry.Client, nil
+	}
+	f.Appliance = func(c *configuration.Config) (*appliance.Appliance, error) {
+		api, _ := f.APIClient(c)
+
+		a := &appliance.Appliance{
+			APIClient:  api,
+			HTTPClient: api.GetConfig().HTTPClient,
+			Token:      "",
+		}
+		return a, nil
+	}
+	cmd := NewBackupAPICmd(f)
+
+	cmd.SetOut(io.Discard)
+	cmd.SetErr(io.Discard)
+
+	_, err := cmd.ExecuteC()
+	if err != nil {
+		t.Fatalf("executeC %s", err)
+	}
+
+	got, err := io.ReadAll(stdout)
+	if err != nil {
+		t.Fatalf("unable to read stdout %s", err)
+	}
+	want := regexp.MustCompile(`Backup API is already enabled`)
+	if !want.MatchString(string(got)) {
+		t.Fatalf("Expected output\n%s\ngot\n%s\n", want, got)
+	}
+}
+
+func TestBackupAPICommand(t *testing.T) {
+	registry := httpmock.NewRegistry()
+	registry.Register(
+		"/global-settings",
+		func(rw http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodGet {
+				rw.Header().Set("Content-Type", "application/json")
+				rw.WriteHeader(http.StatusOK)
+				fmt.Fprint(rw, string(`{
+                    "claimsTokenExpiration": 1440,
+                    "entitlementTokenExpiration": 180,
+                    "administrationTokenExpiration": 720,
+                    "vpnCertificateExpiration": 525600,
+                    "spaMode": "TCP",
+                    "loginBannerMessage": "Authorized use only.",
+                    "messageOfTheDay": "Welcome to Appgate SDP.",
+                    "backupApiEnabled": false,
+                    "fips": false,
+                    "geoIpUpdates": false,
+                    "auditLogPersistenceMode": "Default",
+                    "appDiscoveryDomains": [
+                      "company.com"
+                    ],
+                    "collectiveId": "4c07bc69-57ea-42dd-b702-c2d6c45419fc"
+                  }
+                `))
+			}
+		},
+	)
+	defer registry.Teardown()
+	registry.Serve()
+	stdout := &bytes.Buffer{}
+	stdin := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	in := io.NopCloser(stdin)
+	f := &factory.Factory{
+		Config: &configuration.Config{
+			Debug: false,
+			URL:   fmt.Sprintf("http://localhost:%d", registry.Port),
+		},
+		IOOutWriter: stdout,
+		Stdin:       in,
+		StdErr:      stderr,
+	}
+	f.APIClient = func(c *configuration.Config) (*openapi.APIClient, error) {
+		return registry.Client, nil
+	}
+	f.Appliance = func(c *configuration.Config) (*appliance.Appliance, error) {
+		api, _ := f.APIClient(c)
+
+		a := &appliance.Appliance{
+			APIClient:  api,
+			HTTPClient: api.GetConfig().HTTPClient,
+			Token:      "",
+		}
+		return a, nil
+	}
+	cmd := NewBackupAPICmd(f)
+
+	cmd.SetOut(io.Discard)
+	cmd.SetErr(io.Discard)
+	stubber, teardown := prompt.InitAskStubber()
+	defer teardown()
+	foo := func(prompt *prompt.AskStubber) {
+		prompt.StubOne("newBackupPassphrase")
+	}
+	foo(stubber)
+
+	_, err := cmd.ExecuteC()
+	if err != nil {
+		t.Fatalf("executeC %s", err)
+	}
+
+	got, err := io.ReadAll(stdout)
+	if err != nil {
+		t.Fatalf("unable to read stdout %s", err)
+	}
+	want := regexp.MustCompile(`Backup API and phassphrase has been updated`)
+	if !want.MatchString(string(got)) {
+		t.Fatalf("Expected output\n%s\ngot\n%s\n", want, got)
+	}
+}
