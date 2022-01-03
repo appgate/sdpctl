c569714c1d8d294f4d5939df4d09b47b0f65a1e2
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-01-03 16:02:03 +0100

    Format HTTP validation errors pretty for CLI
    wrap each message as a multierror for prettier response in the cli.

--- pkg/api/apiutil.go
+++ pkg/api/apiutil.go
@@ -2,12 +2,13 @@ package api
 
 import (
 	"encoding/json"
-	"errors"
+	stderrors "errors"
 	"fmt"
 	"io"
 	"net/http"
 
 	"github.com/appgate/appgatectl/pkg/util"
+	"github.com/hashicorp/go-multierror"
 )
 
 type GenericErrorResponse struct {
@@ -24,21 +25,21 @@ func HTTPErrorResponse(response *http.Response, err error) error {
 	if response == nil {
 		return fmt.Errorf("No response %w", err)
 	}
+	var errors error
 	if util.InBetween(response.StatusCode, 400, 499) {
 		responseBody, errRead := io.ReadAll(response.Body)
 		if errRead != nil {
-			return errRead
+			return fmt.Errorf("%d Could not read response body %w", response.StatusCode, err)
 		}
 		errBody := GenericErrorResponse{}
 		if err := json.Unmarshal(responseBody, &errBody); err != nil {
-			return err
+			return fmt.Errorf("%d %w", response.StatusCode, err)
 		}
-		// TODO custom error on http.StatusConflict,  StatusNotAcceptable ?
-		s := errBody.Message
+		errors = multierror.Append(errors, stderrors.New(errBody.Message))
 		for _, e := range errBody.Errors {
-			s = s + " " + e.Field + " " + e.Message
+			errors = multierror.Append(errors, fmt.Errorf("%s %s", e.Field, e.Message))
 		}
-		return errors.New(s)
+		return errors
 	}
-	return nil
+	return multierror.Append(errors, err)
 }
