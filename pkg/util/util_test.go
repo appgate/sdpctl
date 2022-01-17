f9d7aa7aa7386ce22d01b24ca8d04befde42a1cc
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-01-17 12:30:45 +0100

    unit test TestIsJSON

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,6 +1,8 @@
 package util
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestIsValidURL(t *testing.T) {
 	type args struct {
@@ -37,6 +39,51 @@ func TestIsValidURL(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsValidURL(tt.args.addr); got != tt.want {
 				t.Errorf("IsValidURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsJSON(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "valid json",
+			args: args{
+				`
+            {
+                "foo": "bar"
+            }
+            `,
+			},
+			want: true,
+		},
+		{
+			name: "incomplete json",
+			args: args{
+				`
+            {
+                "foo": "bar
+            `,
+			},
+			want: false,
+		},
+		{
+			name: "empty string",
+			args: args{""},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsJSON(tt.args.str); got != tt.want {
+				t.Errorf("IsJSON() = %v, want %v", got, tt.want)
 			}
 		})
 	}
