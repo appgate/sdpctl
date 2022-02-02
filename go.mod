4030e19ac9e781ad230f44ba84246a82cb37196b
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-02-02 18:52:48 +0100

    Windows store bearer token file encrypted with DPAPI
    bearer token is stored in a file %APPDATA%/appgatectl
    username and password (Optional) are stored in windows credentials manager

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.2
 	github.com/appgate/sdp-api-client-go v1.0.7-0.20211229102702-55d570847963
+	github.com/billgraziano/dpapi v0.4.0
 	github.com/briandowns/spinner v1.18.1
 	github.com/cenkalti/backoff/v4 v4.1.2
 	github.com/cheynewallace/tabby v1.1.1
--- pkg/keyring/keyring.go
+++ pkg/keyring/keyring.go
@@ -1,34 +1,7 @@
+//go:build !windows
+// +build !windows
+
 package keyring
-
-import (
-	"fmt"
-
-	"github.com/appgate/appgatectl/pkg/hashcode"
-	zkeyring "github.com/zalando/go-keyring"
-)
-
-const (
-	keyringService = "appgatectl"
-	password       = "password"
-	username       = "username"
-	bearer         = "bearer"
-)
-
-func getSecret(key string) (string, error) {
-	secret, err := zkeyring.Get(keyringService, key)
-	if err != nil {
-		return "", nil
-	}
-	return secret, nil
-}
-
-func format(prefix, value string) string {
-	return fmt.Sprintf("%d.%s", hashcode.String(prefix), value)
-}
-
-func setSecret(key, value string) error {
-	return zkeyring.Set(keyringService, key, value)
-}
 
 func GetPassword(prefix string) (string, error) {
 	return getSecret(format(prefix, password))
