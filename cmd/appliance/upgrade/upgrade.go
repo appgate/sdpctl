143ed9783de6b629b88df1df734a2fa706ef3a26
Lars Kajes <lars.kajes@appgate.com>
2022-02-23 12:14:22 +0100

    Add timeout and batch-size flags to upgrade

--- cmd/appliance/upgrade/upgrade.go
+++ cmd/appliance/upgrade/upgrade.go
@@ -1,16 +1,18 @@
 package upgrade
 
 import (
+	"time"
+
 	"github.com/appgate/sdpctl/pkg/factory"
 	"github.com/spf13/cobra"
 )
 
 // NewUpgradeCmd return a new upgrade command
 func NewUpgradeCmd(f *factory.Factory) *cobra.Command {
-
 	var upgradeCmd = &cobra.Command{
-		Use:   "upgrade",
-		Short: "Perform appliance upgrade on the Appgate SDP Collective",
+		Use:              "upgrade",
+		TraverseChildren: true,
+		Short:            "Perform appliance upgrade on the Appgate SDP Collective",
 		Long: `The upgrade procedure is divided into two parts,
   - prepare: Upload the image new appliance image to the Appgate SDP Collective.
   - complete: Install a prepared upgrade on the secondary partition and perform a reboot to make the second partition the primary.
@@ -25,6 +27,10 @@ Additional subcommands included are:
 	upgradeCmd.AddCommand(NewPrepareUpgradeCmd(f))
 	upgradeCmd.AddCommand(NewUpgradeCancelCmd(f))
 	upgradeCmd.AddCommand(NewUpgradeCompleteCmd(f))
+
+	flags := upgradeCmd.PersistentFlags()
+	flags.Duration("timeout", 10*time.Minute, "Timeout for the upgrade operation. The timeout applies to each appliance which is being operated on.")
+	flags.Int("batch-size", 5, "Upgrade is done in batches. You can control the batch size using this flag.")
 
 	return upgradeCmd
 }
