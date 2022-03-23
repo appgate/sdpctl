8a702516dbdbc49a6b38e580ba1e9ade4b1393d9
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-03-23 15:44:02 +0100

    dont attempt to prepare offline appliances
    offline appliances will automatically be excluded from upgrade prepare.

--- cmd/appliance/upgrade/prepare.go
+++ cmd/appliance/upgrade/prepare.go
@@ -186,7 +186,7 @@ func prepareRun(cmd *cobra.Command, args []string, opts *prepareUpgradeOptions) 
 	if err != nil {
 		return err
 	}
-	appliances := appliancepkg.FilterAppliances(Allappliances, filter)
+	filteredAppliances := appliancepkg.FilterAppliances(Allappliances, filter)
 
 	primaryController, err := appliancepkg.FindPrimaryController(Allappliances, host)
 	if err != nil {
@@ -197,6 +197,8 @@ func prepareRun(cmd *cobra.Command, args []string, opts *prepareUpgradeOptions) 
 	if err != nil {
 		return err
 	}
+	appliances, _, _ := appliancepkg.FilterAvailable(filteredAppliances, initialStats.GetData())
+
 	if hasLowDiskSpace := appliancepkg.HasLowDiskSpace(initialStats.GetData()); len(hasLowDiskSpace) > 0 {
 		appliancepkg.PrintDiskSpaceWarningMessage(opts.Out, hasLowDiskSpace)
 		if !opts.NoInteractive {
