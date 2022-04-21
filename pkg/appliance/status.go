eac70410194ed358d66769cd711025ae76077de9
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-04-21 15:04:06 +0200

    refactor, upgradeStatus to two methods
    - UpgradeStatus should be able to check more then 1 status message, use slice as desired state.
    - upgradeStatusSubscribe allow extra parameter for channel to report back to.
    - upgradeStatus same as upgradeStatusSubscribe, but without channel. (TODO; another refactor?)

--- pkg/appliance/status.go
+++ pkg/appliance/status.go
@@ -14,7 +14,11 @@ import (
 )
 
 type WaitForUpgradeStatus interface {
-	Wait(ctx context.Context, appliance openapi.Appliance, desiredStatus string, current chan<- string) error
+	// Wait does expodential backoff retries on upgrade status and return nil if it reaches any of the desiredStatuses
+	Wait(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string) error
+	// Subscribe does expodential backoff retries on upgrade status until it reaches a desiredStatuses and reports it to current <- string
+	Subscribe(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string, current chan<- string) error
+
 	Watch(ctx context.Context, p *mpb.Progress, appliance openapi.Appliance, endState string, failState string, current <-chan string)
 }
 
@@ -32,7 +36,7 @@ var defaultExponentialBackOff = &backoff.ExponentialBackOff{
 	Clock:               backoff.SystemClock,
 }
 
-func (u *UpgradeStatus) upgradeStatus(ctx context.Context, appliance openapi.Appliance, desiredStatus string, currentStatus chan<- string) backoff.Operation {
+func (u *UpgradeStatus) upgradeStatusSubscribe(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string, currentStatus chan<- string) backoff.Operation {
 	fields := log.Fields{"appliance": appliance.GetName()}
 	return func() error {
 		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
@@ -53,23 +57,69 @@ func (u *UpgradeStatus) upgradeStatus(ctx context.Context, appliance openapi.App
 		}
 		fields["image"] = status.GetDetails()
 		fields["status"] = s
-		log.WithFields(fields).Infof("waiting for '%s' state", desiredStatus)
-		if s == desiredStatus {
+		log.WithFields(fields).Infof("waiting for '%s' state", desiredStatuses)
+		if util.InSlice(s, desiredStatuses) {
 			return nil
 		}
 		return fmt.Errorf(
 			"%s never reached %s, got %q %s",
 			appliance.GetName(),
-			desiredStatus,
+			desiredStatuses,
 			s,
 			status.GetDetails(),
 		)
 	}
 }
 
-func (u *UpgradeStatus) Wait(ctx context.Context, appliance openapi.Appliance, desiredStatus string, current chan<- string) error {
+func (u *UpgradeStatus) upgradeStatus(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string) backoff.Operation {
+	fields := log.Fields{"appliance": appliance.GetName()}
+	return func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+		status, err := u.Appliance.UpgradeStatus(ctx, appliance.GetId())
+		if err != nil {
+			return err
+		}
+		var s string
+		if v, ok := status.GetStatusOk(); ok {
+			s = *v
+			responseStatus := status.GetStatus()
+			if responseStatus == UpgradeStatusFailed {
+				return backoff.Permanent(fmt.Errorf("Upgrade failed on %s - %s", appliance.GetName(), status.GetDetails()))
+			}
+		}
+		fields["image"] = status.GetDetails()
+		fields["status"] = s
+		log.WithFields(fields).Infof("waiting for '%s' state", desiredStatuses)
+		if util.InSlice(s, desiredStatuses) {
+			return nil
+		}
+		return fmt.Errorf(
+			"%s never reached %s, got %q %s",
+			appliance.GetName(),
+			desiredStatuses,
+			s,
+			status.GetDetails(),
+		)
+	}
+}
+
+func (u *UpgradeStatus) Subscribe(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string, current chan<- string) error {
 	b := backoff.WithContext(defaultExponentialBackOff, ctx)
-	if err := backoff.Retry(u.upgradeStatus(ctx, appliance, desiredStatus, current), b); err != nil {
+	if err := backoff.Retry(u.upgradeStatusSubscribe(ctx, appliance, desiredStatuses, current), b); err != nil {
+		return err
+	}
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		return nil
+	}
+}
+
+func (u *UpgradeStatus) Wait(ctx context.Context, appliance openapi.Appliance, desiredStatuses []string) error {
+	b := backoff.WithContext(defaultExponentialBackOff, ctx)
+	if err := backoff.Retry(u.upgradeStatus(ctx, appliance, desiredStatuses), b); err != nil {
 		return err
 	}
 	select {
