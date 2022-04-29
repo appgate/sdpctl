4a62f28b79f1c7588ad9c7d41e36b19ed740efd0
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-04-29 11:13:54 +0200

    queue test fix race condition

--- pkg/queue/queue_test.go
+++ pkg/queue/queue_test.go
@@ -5,57 +5,33 @@ import (
 	"testing"
 )
 
-func TestQueue(t *testing.T) {
-	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	type args struct {
-		capacity int
-		workers  int
-		closure  Closure
-		items    []string
+var alphabet = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+func TestQueueWithError(t *testing.T) {
+	qw := New(len(alphabet), 2)
+	for _, letter := range alphabet {
+		qw.Push(letter)
 	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "process alphabet queue with one error",
-			args: args{
-				capacity: len(alphabet),
-				workers:  2,
-				items:    alphabet,
-				closure: func(v interface{}) error {
-					if v.(string) == "e" {
-						return errors.New("E is error letter")
-					}
-					return nil
-				},
-			},
-			wantErr: true,
-		},
-		{
-			name: "process alphabet queue with no errors",
-			args: args{
-				capacity: len(alphabet),
-				workers:  2,
-				items:    alphabet,
-				closure: func(v interface{}) error {
-					return nil
-				},
-			},
-			wantErr: false,
-		},
+	err := qw.Work(func(v interface{}) error {
+		if v.(string) == "e" {
+			return errors.New("E is error letter")
+		}
+		return nil
+	})
+	if err == nil {
+		t.Fatalf("Expected error, got none")
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			qw := New(tt.args.capacity, tt.args.workers)
-			for _, letter := range tt.args.items {
-				qw.Push(letter)
-			}
-			err := qw.Work(tt.args.closure)
-			if (err != nil) != tt.wantErr {
-				t.Fatalf("Expected %v got %v", tt.wantErr, err)
-			}
-		})
+}
+func TestQueueNoError(t *testing.T) {
+	qw := New(len(alphabet), 2)
+	for _, letter := range alphabet {
+		qw.Push(letter)
+	}
+	qw.queue.Unlock()
+	err := qw.Work(func(v interface{}) error {
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("Expected no error, got %s", err)
 	}
 }
