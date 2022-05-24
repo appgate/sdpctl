2c54da38b4bd530ee86baeb3ab75e75d98ff3b73
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-05-24 08:45:20 +0200

    updated copyright notice

--- pkg/terminal/read_bsd.go
+++ pkg/terminal/read_bsd.go
@@ -1,7 +1,12 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 //go:build (darwin || freebsd || netbsd || openbsd || dragonfly) && !appengine
 // +build darwin freebsd netbsd openbsd dragonfly
 // +build !appengine
 
+// https://github.com/golang/term/blob/e5f449aeb1717c7a4156811d5d65510fee9d1530/term_unix_bsd.go
 package terminal
 
 import "syscall"
--- pkg/terminal/read_sysv.go
+++ pkg/terminal/read_sysv.go
@@ -1,4 +1,4 @@
-// Copyright 2013 The Go Authors. All rights reserved.
+// Copyright 2021 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -6,6 +6,7 @@
 // +build linux solaris aix zos
 // +build !appengine
 
+// https://github.com/golang/term/blob/c04ba851c2a451287ce12942595152e208390af9/term_unix_other.go
 package terminal
 
 import "golang.org/x/sys/unix"
