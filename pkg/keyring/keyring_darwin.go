425e98b81b66eb29a581106587311cf3e0cbeb5e
Masaya Ando <masaya.ando@appgate.com>
2022-07-29 17:21:09 -0400

    Use go-keychain for macOS keychain handling

--- pkg/keyring/keyring_darwin.go
+++ pkg/keyring/keyring_darwin.go
@@ -1,28 +1,63 @@
 //go:build darwin
 // +build darwin
 
-// The bearer token is not saved in the OS keychain for macOS because of string length limitation.
-// If the user choose to save their username/password in the keychain, they will still experiance a
-// seemless integration, however they need todo a few more http requests on startup for each command they execute.
 package keyring
 
 import (
 	"errors"
+	"fmt"
+	"github.com/keybase/go-keychain"
 	"os"
-
-	zkeyring "github.com/zalando/go-keyring"
 )
 
 // ClearCredentials removes any existing items in the keychain,
 // it will ignore if not found errors
 func ClearCredentials(prefix string) error {
-	for _, k := range []string{username, password} {
-		if err := deleteSecret(format(prefix, k)); err != nil {
-			if !errors.Is(err, zkeyring.ErrNotFound) {
-				return err
-			}
-
+	for _, k := range []string{username, password, bearer} {
+		item := keychain.NewItem()
+		item.SetSecClass(keychain.SecClassGenericPassword)
+		item.SetService(keyringService)
+		item.SetAccount(format(prefix, k))
+		err := keychain.DeleteItem(item)
+		if err != nil {
+			return errors.New("failed to delete credential from keychain")
 		}
+	}
+	return nil
+}
+
+func QueryKeychain(key string) (string, error) {
+	query := keychain.NewItem()
+	query.SetService(keyringService)
+	query.SetSecClass(keychain.SecClassGenericPassword)
+	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetAccessGroup(keyringService)
+	query.SetAccount(key)
+	query.SetReturnAttributes(true)
+	query.SetReturnData(true)
+	result, err := keychain.QueryItem(query)
+	if err != nil {
+		return "", errors.New("encountered error when querying the keychain")
+	}
+	if len(result) != 1 {
+		return "", errors.New(fmt.Sprintf("could not find key: %s", key))
+	}
+	return string(result[0].Data), nil
+}
+
+func AddKeychain(key string, value string) error {
+	item := keychain.NewItem()
+	item.SetService(keyringService)
+	item.SetSecClass(keychain.SecClassGenericPassword)
+	item.SetMatchLimit(keychain.MatchLimitOne)
+	item.SetAccessGroup(keyringService)
+	item.SetAccount(key)
+	item.SetData([]byte(value))
+	item.SetSynchronizable(keychain.SynchronizableNo)
+	item.SetAccessible(keychain.AccessibleWhenUnlocked)
+	err := keychain.AddItem(item)
+	if err != nil {
+		return err
 	}
 	return nil
 }
@@ -31,50 +66,68 @@ func GetPassword(prefix string) (string, error) {
 	if v, ok := os.LookupEnv("SDPCTL_PASSWORD"); ok {
 		return v, nil
 	}
-	return getSecret(format(prefix, password))
+	pw, err := QueryKeychain(format(prefix, password))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("failed to get password from keychain: %s", err))
+	}
+	return pw, nil
 }
 
 func SetPassword(prefix, secret string) error {
-	err := setSecret(format(prefix, password), secret)
+	err := AddKeychain(format(prefix, password), secret)
 	if err != nil {
-		os.Setenv("SDPCTL_PASSWORD", secret)
-		return nil
+		return err
 	}
-	return err
+	return nil
 }
 
 func GetBearer(prefix string) (string, error) {
-	if v, ok := os.LookupEnv("SDPCTL_BEARER"); ok {
-		return v, nil
+	token, err := QueryKeychain(format(prefix, bearer))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("failed to get bearer token from keychain: %s", err))
 	}
-	return "", errors.New("bearer token not saved persistently on macOS")
+	return token, nil
 }
 
 func SetBearer(prefix, secret string) error {
-	os.Setenv("SDPCTL_BEARER", secret)
+	err := AddKeychain(format(prefix, bearer), secret)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func SetUsername(prefix, secret string) error {
-	err := setSecret(format(prefix, username), secret)
+	err := AddKeychain(format(prefix, username), secret)
 	if err != nil {
-		os.Setenv("SDPCTL_USERNAME", secret)
-		return nil
+		return err
 	}
-	return err
+	return nil
 }
 
 func GetUsername(prefix string) (string, error) {
 	if v, ok := os.LookupEnv("SDPCTL_USERNAME"); ok {
 		return v, nil
 	}
-	return getSecret(format(prefix, username))
+	user, err := QueryKeychain(format(prefix, username))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("failed to get username from keychain: %s", err))
+	}
+	return user, nil
 }
 
 func GetRefreshToken(prefix string) (string, error) {
-	return "", errors.New("macOS is not supported")
+	token, err := QueryKeychain(format(prefix, refreshToken))
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("failed to get refresh token from keychain: %s", err))
+	}
+	return token, nil
 }
 
 func SetRefreshToken(prefix, secret string) error {
-	return errors.New("macOS is not supported")
+	err := AddKeychain(format(prefix, refreshToken), secret)
+	if err != nil {
+		return err
+	}
+	return nil
 }
