4f3e192d16ad0d4b20d823c0f586452a14774f82
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-09-02 14:53:59 +0200

    staticcheck 1.19
    pkg/token/api.go:43:6: this comparison is always true (SA4023)
    pkg/token/api.go:42:14: the lhs of the comparison is the 1st return value of this function call
    
    wrap all http errors in token/api.go in api.HTTPErrorResponse(response, err)

--- pkg/token/api.go
+++ pkg/token/api.go
@@ -15,43 +15,33 @@ type Token struct {
 }
 
 func (t *Token) ListDistinguishedNames(ctx context.Context) ([]openapi.DistinguishedName, error) {
-	dn, _, err := t.APIClient.ActiveDevicesApi.TokenRecordsDnGet(ctx).Authorization(t.Token).Execute()
+	dn, response, err := t.APIClient.ActiveDevicesApi.TokenRecordsDnGet(ctx).Authorization(t.Token).Execute()
 	if err != nil {
-		return nil, err
+		return nil, api.HTTPErrorResponse(response, err)
 	}
 	return dn.GetData(), nil
 }
 
 func (t *Token) RevokeByDistinguishedName(request openapi.ApiTokenRecordsRevokedByDnDistinguishedNamePutRequest, body openapi.TokenRevocationRequest) (*http.Response, error) {
 	_, response, err := request.Authorization(t.Token).TokenRevocationRequest(body).Execute()
-
 	if err != nil {
-		httpErr := api.HTTPErrorResponse(response, err)
-		if httpErr != nil {
-			return nil, httpErr
-		}
-		return nil, err
+		return nil, api.HTTPErrorResponse(response, err)
 	}
 	return response, nil
 }
 
 func (t *Token) RevokeByTokenType(request openapi.ApiTokenRecordsRevokedByTypeTokenTypePutRequest, body openapi.TokenRevocationRequest) (*http.Response, error) {
 	_, response, err := request.Authorization(t.Token).TokenRevocationRequest(body).Execute()
-
 	if err != nil {
-		httpErr := api.HTTPErrorResponse(response, err)
-		if httpErr != nil {
-			return nil, httpErr
-		}
-		return nil, err
+		return nil, api.HTTPErrorResponse(response, err)
 	}
 	return response, nil
 }
 
 func (t *Token) ReevaluateByDistinguishedName(ctx context.Context, dn string) ([]string, error) {
-	reevaluatedDn, _, err := t.APIClient.ActiveDevicesApi.TokenRecordsReevalByDnDistinguishedNamePost(ctx, dn).Authorization(t.Token).Execute()
+	reevaluatedDn, response, err := t.APIClient.ActiveDevicesApi.TokenRecordsReevalByDnDistinguishedNamePost(ctx, dn).Authorization(t.Token).Execute()
 	if err != nil {
-		return nil, err
+		return nil, api.HTTPErrorResponse(response, err)
 	}
 	return reevaluatedDn.GetReevaluatedDistinguishedNames(), nil
 }
