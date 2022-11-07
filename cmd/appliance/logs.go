af75e2f015b668cef5f1519806fad3d1405df52f
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-11-07 10:50:23 +0100

    new cmd: sdpctl appliance logs
    download appliance log zip bundle

---
+++ cmd/appliance/logs.go
@@ -0,0 +1,160 @@
+package appliance
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/appgate/sdpctl/pkg/api"
+	"github.com/appgate/sdpctl/pkg/cmdappliance"
+	"github.com/appgate/sdpctl/pkg/factory"
+	"github.com/appgate/sdpctl/pkg/terminal"
+	"github.com/appgate/sdpctl/pkg/tui"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/vbauerster/mpb/v7"
+	"github.com/vbauerster/mpb/v7/decor"
+)
+
+type logOpts struct {
+	cmdappliance.AppliancCmdOpts
+	Out        io.Writer
+	BaseURL    string
+	HTTPClient func() (*http.Client, error)
+	SpinnerOut func() io.Writer
+	Version    int
+	Path       string
+}
+
+func NewLogsCmd(f *factory.Factory) *cobra.Command {
+	aopts := cmdappliance.AppliancCmdOpts{
+		Appliance: f.Appliance,
+		Config:    f.Config,
+		CanPrompt: f.CanPrompt(),
+	}
+	opts := logOpts{
+		aopts,
+		f.IOOutWriter,
+		f.Config.URL,
+		f.CustomHTTPClient,
+		f.GetSpinnerOutput(),
+		f.Config.Version,
+		"",
+	}
+	cmd := &cobra.Command{
+		Use: "logs",
+		Args: func(cmd *cobra.Command, args []string) error {
+			return cmdappliance.ArgsSelectAppliance(cmd, args, &opts.AppliancCmdOpts)
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			return logsRun(c, args, &opts)
+		},
+	}
+	cmd.Flags().StringVarP(&opts.Path, "path", "", "", "Optional path to write to")
+	return cmd
+}
+
+func logsRun(cmd *cobra.Command, args []string, opts *logOpts) error {
+	terminal.Lock()
+	defer terminal.Unlock()
+	client, err := opts.HTTPClient()
+	if err != nil {
+		return fmt.Errorf("could not resolve a HTTP client based on your current config %s", err)
+	}
+	path, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	if len(opts.Path) > 0 {
+		path = opts.Path
+	}
+
+	requestURL := fmt.Sprintf("%s/appliances/%s/logs", opts.BaseURL, opts.ApplianceID)
+	request, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	if err != nil {
+		return err
+	}
+	request = request.WithContext(context.WithValue(context.Background(), factory.ContextAcceptValue, fmt.Sprintf("application/vnd.appgate.peer-v%d+zip", opts.Version)))
+	log.Infof("starting downloading log zip bundle or %s", opts.ApplianceID)
+	response, err := client.Do(request)
+	if response == nil || err != nil {
+		return api.HTTPErrorResponse(response, err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return api.HTTPErrorResponse(response, err)
+	}
+	defer response.Body.Close()
+
+	name := fmt.Sprintf("%s_logs.zip", opts.ApplianceID)
+	_, params, err := mime.ParseMediaType(response.Header.Get("Content-Disposition"))
+	if err == nil {
+		if v, ok := params["filename"]; ok {
+			name = v
+		}
+	}
+
+	file, err := os.Create(filepath.Join(path, name))
+	if err != nil {
+		return err
+	}
+
+	p := mpb.New(mpb.WithWidth(64), mpb.WithOutput(opts.SpinnerOut()))
+
+	bar := p.New(0,
+		mpb.SpinnerStyle(tui.SpinnerStyle...),
+		mpb.BarFillerOnComplete(tui.Check),
+		mpb.BarWidth(1),
+		mpb.AppendDecorators(decor.Name(name+" "), decor.CurrentKiloByte("% .1f")),
+	)
+
+	size, err := copy(file, response.Body, bar)
+	if err != nil {
+		return err
+	}
+	p.Wait()
+	log.Infof("Downloaded %d bytes zip bundle for %s", size, opts.ApplianceID)
+	fmt.Fprintf(opts.Out, "saved to %s\n", file.Name())
+	return nil
+}
+
+func copy(dst io.Writer, src io.Reader, bar *mpb.Bar) (written int64, err error) {
+	buf := make([]byte, 32*1024)
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			// increment methods won't trigger complete event because bar was constructed with total = 0
+			bar.IncrBy(nr)
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = errors.New("invalid write result")
+				}
+			}
+			written += int64(nw)
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er == io.EOF {
+				// triggering complete event now
+				bar.SetTotal(-1, true)
+			} else if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
+}
---
+++ cmd/appliance/logs_test.go
@@ -0,0 +1,67 @@
+package appliance
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/appgate/sdp-api-client-go/api/v17/openapi"
+	"github.com/appgate/sdpctl/pkg/appliance"
+	"github.com/appgate/sdpctl/pkg/configuration"
+	"github.com/appgate/sdpctl/pkg/factory"
+	"github.com/appgate/sdpctl/pkg/httpmock"
+)
+
+func TestNewLogsCmd(t *testing.T) {
+	dir := t.TempDir()
+	registry := httpmock.NewRegistry(t)
+	registry.Register(
+		"/appliances/20e75a08-96c6-4ea3-833e-cdbac346e2ae/logs",
+		func(rw http.ResponseWriter, r *http.Request) {
+			if r.Method == http.MethodGet {
+				rw.Header().Set("Content-Type", "application/vnd.appgate.peer-v18+zip")
+				rw.WriteHeader(http.StatusOK)
+				fmt.Fprint(rw, string(`binary data`))
+			}
+		},
+	)
+
+	defer registry.Teardown()
+	registry.Serve()
+	stdout := &bytes.Buffer{}
+
+	stderr := &bytes.Buffer{}
+	f := &factory.Factory{
+		Config: &configuration.Config{
+			Debug: false,
+			URL:   fmt.Sprintf("http://localhost:%d", registry.Port),
+		},
+		IOOutWriter: stdout,
+		StdErr:      stderr,
+	}
+	f.APIClient = func(c *configuration.Config) (*openapi.APIClient, error) {
+		return registry.Client, nil
+	}
+	f.CustomHTTPClient = func() (*http.Client, error) {
+		return &http.Client{}, nil
+	}
+	f.Appliance = func(c *configuration.Config) (*appliance.Appliance, error) {
+		api, _ := f.APIClient(c)
+
+		a := &appliance.Appliance{
+			APIClient:  api,
+			HTTPClient: api.GetConfig().HTTPClient,
+			Token:      "",
+		}
+		return a, nil
+	}
+	cmd := NewLogsCmd(f)
+	cmd.PersistentFlags().Bool("no-interactive", false, "")
+	cmd.SetArgs([]string{"20e75a08-96c6-4ea3-833e-cdbac346e2ae", "--path", dir})
+	_, err := cmd.ExecuteC()
+	if err != nil {
+		t.Fatalf("executeC %s", err)
+	}
+
+}
