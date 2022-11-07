f167fe9a4750b933058edd285ceaa9b40f9cfc5e
Daniel Nilsson <daniel.nilsson@appgate.com>
2022-11-07 09:39:30 +0100

    permanently error if we get failure result body

--- pkg/appliance/change/change.go
+++ pkg/appliance/change/change.go
@@ -41,6 +41,12 @@ func (ac *ApplianceChange) RetryUntilCompleted(ctx context.Context, changeID, ap
 			}
 			return backoff.Permanent(fmt.Errorf("appliance change failed on appliance id %s %s %s", applianceID, change.GetResult(), change.GetDetails()))
 		}
+		if change.GetResult() == "failure" {
+			if v, ok := change.GetDetailsOk(); ok && len(*v) > 0 {
+				return backoff.Permanent(fmt.Errorf("unable to apply change %s", *v))
+			}
+			return backoff.Permanent(fmt.Errorf("appliance change failed %s %s", change.GetResult(), change.GetDetails()))
+		}
 		if change.GetResult() != "success" && change.GetStatus() == "completed" {
 			return fmt.Errorf("Got result %s and status %s", change.GetResult(), change.GetStatus())
 		}
