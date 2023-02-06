1d090b3c4dcb92b170fce78272cf1ec952184da3
Lars Kajes <lars.kajes@appgate.com>
2023-02-06 14:23:13 +0100

    move test helper function

--- pkg/appliance/functions_test.go
+++ pkg/appliance/functions_test.go
@@ -5,10 +5,10 @@ import (
 	"reflect"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/appgate/sdp-api-client-go/api/v18/openapi"
 	"github.com/appgate/sdpctl/pkg/hashcode"
-	tx "github.com/appgate/sdpctl/testing"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/uuid"
@@ -2808,11 +2808,11 @@ func Test_sortAppliances(t *testing.T) {
 	}
 
 	// Generate appliances
-	app1, _ := tx.GenerateApplianceWithStats([]int{tx.FunctionController}, "controller1", "primary.appgate.com", "6.1.1-12345", "healthy")
-	app2, _ := tx.GenerateApplianceWithStats([]int{tx.FunctionController}, "controller2", "secondary.appgate.com", "6.1.1-12345", "healthy")
-	app3, _ := tx.GenerateApplianceWithStats([]int{tx.FunctionController}, "controller3", "backup1.appgate.com", "6.1.1-12345", "healthy")
-	app4, _ := tx.GenerateApplianceWithStats([]int{tx.FunctionController}, "controller4", "backup2.appgate.com", "6.1.1-12345", "healthy")
-	app5, _ := tx.GenerateApplianceWithStats([]int{tx.FunctionController}, "controller5", "balance1.appgate.com", "6.1.1-12345", "healthy")
+	app1, _ := GenerateApplianceWithStats([]string{FunctionController}, "controller1", "primary.appgate.com", "6.1.1-12345", "healthy")
+	app2, _ := GenerateApplianceWithStats([]string{FunctionController}, "controller2", "secondary.appgate.com", "6.1.1-12345", "healthy")
+	app3, _ := GenerateApplianceWithStats([]string{FunctionController}, "controller3", "backup1.appgate.com", "6.1.1-12345", "healthy")
+	app4, _ := GenerateApplianceWithStats([]string{FunctionController}, "controller4", "backup2.appgate.com", "6.1.1-12345", "healthy")
+	app5, _ := GenerateApplianceWithStats([]string{FunctionController}, "controller5", "balance1.appgate.com", "6.1.1-12345", "healthy")
 
 	// Modify appliances for tests
 	app3.SetActivated(false)
@@ -2886,4 +2886,75 @@ func Test_sortAppliances(t *testing.T) {
 			assert.Equal(t, got, tt.want)
 		})
 	}
+}
+
+func GenerateApplianceWithStats(activeFunctions []string, name, hostname, version, status string) (openapi.Appliance, openapi.StatsAppliancesListAllOfData) {
+	id := uuid.NewString()
+	now := time.Now()
+	ctrl := &openapi.ApplianceAllOfController{}
+	ls := &openapi.ApplianceAllOfLogServer{}
+	gw := &openapi.ApplianceAllOfGateway{}
+	lf := &openapi.ApplianceAllOfLogForwarder{}
+	con := &openapi.ApplianceAllOfConnector{}
+	portal := &openapi.Portal{}
+
+	for _, f := range activeFunctions {
+		switch f {
+		case FunctionController:
+			ctrl.SetEnabled(true)
+		case FunctionGateway:
+			gw.SetEnabled(true)
+		case FunctionLogServer:
+			ls.SetEnabled(true)
+		case FunctionLogForwarder:
+			lf.SetEnabled(true)
+		case FunctionPortal:
+			portal.SetEnabled(true)
+		case FunctionConnector:
+			con.SetEnabled(true)
+		}
+	}
+
+	app := openapi.Appliance{
+		Id:                                   openapi.PtrString(id),
+		Name:                                 name,
+		Notes:                                nil,
+		Created:                              openapi.PtrTime(now),
+		Updated:                              openapi.PtrTime(now),
+		Tags:                                 []string{},
+		ConnectToPeersUsingClientPortWithSpa: nil,
+		PeerInterface:                        &openapi.ApplianceAllOfPeerInterface{},
+		Activated:                            openapi.PtrBool(true),
+		PendingCertificateRenewal:            openapi.PtrBool(false),
+		Version:                              openapi.PtrInt32(18),
+		Hostname:                             hostname,
+		Site:                                 openapi.PtrString("Default Site"),
+		SiteName:                             new(string),
+		Customization:                        new(string),
+		ClientInterface:                      openapi.ApplianceAllOfClientInterface{},
+		AdminInterface: &openapi.ApplianceAllOfAdminInterface{
+			Hostname:  hostname,
+			HttpsPort: openapi.PtrInt32(8443),
+		},
+		Networking:          openapi.ApplianceAllOfNetworking{},
+		Ntp:                 &openapi.ApplianceAllOfNtp{},
+		SshServer:           &openapi.ApplianceAllOfSshServer{},
+		SnmpServer:          &openapi.ApplianceAllOfSnmpServer{},
+		HealthcheckServer:   &openapi.ApplianceAllOfHealthcheckServer{},
+		PrometheusExporter:  &openapi.ApplianceAllOfPrometheusExporter{},
+		Ping:                &openapi.ApplianceAllOfPing{},
+		LogServer:           ls,
+		Controller:          ctrl,
+		Gateway:             gw,
+		LogForwarder:        lf,
+		Connector:           con,
+		Portal:              portal,
+		RsyslogDestinations: []openapi.ApplianceAllOfRsyslogDestinations{},
+		HostnameAliases:     []string{},
+	}
+	appstatdata := *openapi.NewStatsAppliancesListAllOfDataWithDefaults()
+	appstatdata.SetId(app.GetId())
+	appstatdata.SetStatus(status)
+	appstatdata.SetVersion(version)
+	return app, appstatdata
 }
