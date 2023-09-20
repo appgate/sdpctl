1d906ebebd035b395962f9f6fccdc23f5405170e
Lars Kajes <lars.kajes@appgate.com>
2023-09-20 14:41:52 +0200

    remove confusing comment

--- pkg/tui/style_windows.go
+++ pkg/tui/style_windows.go
@@ -7,7 +7,6 @@ var (
 	// SpinnerStyle for Windows has no special unicode characters, to support cmd.exe out-of-the-box.
 	SpinnerStyle []string = []string{"-", "\\", "|", "/"}
 
-	// SpinnerDone intentionally left empty due to causing false positives in cmd.exe
 	Check string = "[COMPLETE]"
 	Cross string = "[ERROR]"
 	Yes   string = "Y"
