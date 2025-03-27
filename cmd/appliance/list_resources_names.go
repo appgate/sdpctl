1ab90c33aff69ca6cb5163166a5e526bff9bbaf7
Torger Miller <torger.miller@appgate.com>
2025-03-27 08:07:49 -0400

    [SA-22768] Add initial implementation

---
+++ cmd/appliance/list_resources_names.go
@@ -0,0 +1,114 @@
+package appliance
+
+import (
+	"context"
+	"io"
+
+	"github.com/appgate/sdp-api-client-go/api/v21/openapi"
+	"github.com/appgate/sdpctl/pkg/api"
+	appliancepkg "github.com/appgate/sdpctl/pkg/appliance"
+	"github.com/appgate/sdpctl/pkg/cmdutil"
+	"github.com/appgate/sdpctl/pkg/configuration"
+	"github.com/appgate/sdpctl/pkg/docs"
+	"github.com/appgate/sdpctl/pkg/factory"
+	"github.com/appgate/sdpctl/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+type ResourcesNamesOpts struct {
+	Config      *configuration.Config
+	Out         io.Writer
+	Client      func(c *configuration.Config) (*openapi.APIClient, error)
+	Appliance   func(c *configuration.Config) (*appliancepkg.Appliance, error)
+	debug       bool
+	json        bool
+	siteID string
+}
+
+func ResourceNamesCmd(f *factory.Factory) *cobra.Command {
+	opts := ResourcesNamesOpts{
+		Config:    f.Config,
+		Client:    f.APIClient,
+		Appliance: f.Appliance,
+		debug:     f.Config.Debug,
+		Out:       f.IOOutWriter,
+	}
+	var cmd = &cobra.Command{
+		Use:     "resources",
+		Short:   docs.SitesResourcesDocsList.Short,
+		Long:    docs.SitesResourcesDocsList.Long,
+		Example: docs.SitesResourcesDocsList.ExampleString(),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			// a, err := opts.Appliance(opts.Config)
+			// if err != nil {
+			// 	return err
+			// }
+			// ctx := context.Background()
+			
+			// orderBy, err := cmd.Flags().GetStringSlice("order-by")
+			// if err != nil {
+			// 	return err
+			// }
+			// descending, err := cmd.Flags().GetBool("descending")
+			// if err != nil {
+			// 	return err
+			// }
+			// if len(args) != 1 {
+			// 	opts.siteID, err = appliancepkg.PromptSelect(ctx, a, filter, orderBy, descending)
+			// 	if err != nil {
+			// 		return err
+			// 	}
+			// 	return nil
+			// }
+
+			// Validate UUID if the argument is applied
+			// uuidArg := args[0]
+			// _, err = uuid.Parse(uuidArg)
+			// if err != nil {
+			// 	log.WithField("error", err).Info("Invalid ID")
+			// 	if err != nil {
+			// 		return err
+			// 	}
+			// }
+			// opts.siteID = uuidArg
+
+			return nil
+		},
+		RunE: func(c *cobra.Command, args []string) error {
+			return ResourcesNamesStatusRun(&opts)
+		},
+	}
+	cmd.Flags().BoolVar(&opts.json, "json", false, "Display in JSON format")
+	cmd.SetHelpFunc(cmdutil.HideIncludeExcludeFlags)
+
+	return cmd
+}
+
+func ResourcesNamesStatusRun(opts *ResourcesNamesOpts) error {
+	client, err := opts.Client(opts.Config)
+	if err != nil {
+		return err
+	}
+	token, err := opts.Config.GetBearTokenHeaderValue()
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+
+	result, response, err := client.SitesApi.SitesIdResourcesGet(ctx, opts.siteID).Authorization(token).Execute()
+	if err != nil {
+		return api.HTTPErrorResponse(response, err)
+	}
+	if opts.json {
+		return util.PrintJSON(opts.Out, result)
+	}
+
+	p := util.NewPrinter(opts.Out, 4)
+
+	p.AddHeader("Resource Name")
+	for k := range result.Data {
+		p.AddLine(k)
+	}
+	p.Print()
+	return nil
+}
---
+++ cmd/sites/list_resources.go
@@ -0,0 +1,131 @@
+package sites
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/appgate/sdp-api-client-go/api/v21/openapi"
+	"github.com/appgate/sdpctl/pkg/docs"
+	"github.com/appgate/sdpctl/pkg/util"
+	"github.com/spf13/cobra"
+)
+
+type ResourcesListOptions struct {
+	SitesOptions
+	siteID string
+	json   bool
+}
+
+func NewResourceNamesCmd(parentOpts *SitesOptions) *cobra.Command {
+	opts := &ResourcesListOptions{}
+
+	cmd := &cobra.Command{
+		Use:     "resources <site-id>",
+		Aliases: []string{"ls"},
+		Short:   docs.SitesResourcesDocsList.Short,
+		Long:    docs.SitesResourcesDocsList.Long,
+		Example: docs.SitesResourcesDocsList.ExampleString(),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			opts.SitesAPI = parentOpts.SitesAPI
+			opts.Out = parentOpts.Out
+			opts.CiMode = parentOpts.CiMode
+			opts.NoInteractive = parentOpts.NoInteractive
+			if opts.SitesAPI == nil {
+				return fmt.Errorf("internal error: no sites API available")
+			}
+
+			opts.siteID = args[0]
+
+			sites, err := opts.SitesAPI.ListSites(context.Background())
+			if sites == nil || err != nil{
+				return fmt.Errorf("no sites available")
+
+			}
+
+			// if len(args) > 0 {
+			// 	for _, a := range args {
+			// 		if util.IsUUID(a) {
+			// 			opts.siteId = append(opts.ids, a)
+			// 			continue
+			// 		}
+			// 		opts.siteNames = append(opts.siteNames, a)
+			// 	}
+			// }
+
+
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			
+
+
+
+
+			
+			
+			//resolvers = AllowedResolverTypeEnumValues
+			resolverTypes := openapi.AllowedResolverTypeEnumValues
+			resourceTypes := openapi.AllowedResourceTypeEnumValues
+			resource_return_list := []openapi.ResolverResources{}
+			
+			for resolveType := range resolverTypes{
+				for resourceType := range resourceTypes{
+					resources, err := opts.SitesAPI.ListResources(ctx, opts.siteID, &resolverTypes[resolveType], &resourceTypes[resourceType])
+					if resources != nil {
+						resource_return_list = append(resource_return_list, *resources)
+					// } else {
+					// 	fmt.Fprintln(opts.Out, "No resources found for" + strconv.Itoa(resourceType) + strconv.Itoa(resolveType))
+					 }
+					
+					if err != nil {
+						return err
+						
+					}
+					 	}
+
+			}
+			
+			
+			if len(resource_return_list) <= 0 {
+				fmt.Fprintln(opts.Out, "No resources found in the site")
+				return nil
+			}
+
+
+			if opts.json {
+				o, err := json.MarshalIndent(resource_return_list, "", "  ")
+				if err != nil {
+					return err
+				}
+				fmt.Fprintln(opts.Out, string(o))
+			} else {
+				p := util.NewPrinter(opts.Out, 4)
+				p.AddHeader("Resolver", "Type", "Gateway Name", "Resource Count")
+				for _, s := range resource_return_list {
+
+					p.AddLine(
+						util.StringAbbreviate(string(*s.Resolver)),
+						util.StringAbbreviate(string(*s.Type)),
+						util.StringAbbreviate(string(*s.GatewayName)),
+						util.StringAbbreviate(string(*s.TotalCount)),
+					)
+					p.AddLine("Resources: ")
+					for _, r := range s.Data {
+						p.AddLine(r)
+					}
+
+				}
+				p.Print()
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().BoolVar(&opts.json, "json", false, "")
+
+	return cmd
+}
