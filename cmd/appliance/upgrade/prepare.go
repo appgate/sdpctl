9a7ace5ce57fcec1318c7e1e7c387291739cd72e
Dori Amara <dori.amara@appgate.com>
2025-05-06 15:17:29 -0400

    ZTP-3932 - Exclude multiple autoscaling gateways from upgrade

--- cmd/appliance/upgrade/prepare.go
+++ cmd/appliance/upgrade/prepare.go
@@ -322,11 +322,15 @@ func prepareRun(cmd *cobra.Command, opts *prepareUpgradeOptions) error {
 		// Remove auto-scaled gateways from appliances to be upgraded
 		log.Info("excluding autoscaling gateways")
 		appliancesForUpgrade := make([]openapi.Appliance, 0)
+		// Build a map of autoscaling gateway ids
+		autoscalingGatewayIds := make(map[*string]bool)
+		for _, gw := range gws {
+			autoscalingGatewayIds[gw.Id] = true
+		}
+		// Select appliances that do not match the id of an autoscaling gateway
 		for _, appliance := range appliances {
-			for _, gw := range gws {
-				if appliance.Id != gw.Id {
-					appliancesForUpgrade = append(appliancesForUpgrade, appliance)
-				}
+			if !autoscalingGatewayIds[appliance.Id] {
+				appliancesForUpgrade = append(appliancesForUpgrade, appliance)
 			}
 		}
 		appliances = appliancesForUpgrade
